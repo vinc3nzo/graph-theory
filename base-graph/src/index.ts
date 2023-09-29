7ab459c5405bc480cafd77be283e872f25fbb71f
Evgeny <mangasaryan.ep@gmail.com>
2023-09-29 15:41:52 +0400

    Добавлены примеры графов

--- base-graph/src/index.ts
+++
@@ -1,9 +0,0 @@
-import {Graph} from 'graph/Graph'
-
-const graph = new Graph('graph.json');
-console.log(graph.getAdjacencyList());
-
-graph.removeNode('b')
-console.log(graph.getAdjacencyList())
-
-// graph.dump('out_graph.txt')
